38104449af9431111ceb5618378ba3eb86abf92b
Saw <clsaw@amazon.my>
2023-06-27 16:53:07 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Viewer, Entity, PointGraphics } from "resium";
+import { Cartesian3 } from "cesium";
+
+const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Viewer full>
+      <Entity position={position} name="Tokyo" description="Hello, world.">
+        <PointGraphics pixelSize={10} />
+      </Entity>
+    </Viewer>
   );
 }
 
