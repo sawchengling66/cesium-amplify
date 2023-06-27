43587d5dae611d3caa1b9de170cf7347d38d3d11
Saw <clsaw@amazon.my>
2023-06-27 18:09:55 +0800

    mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import { Viewer, Entity, PointGraphics } from "resium";
 import { Cartesian3 } from "cesium";
 
-const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100);
-
+const origin = Cartesian3.fromDegrees(-95.0, 40.0, 200000.0);
 const cameraDest = Cartesian3.fromDegrees(-95.0, 40.0, 210000);
 const modelMatrix = Transforms.eastNorthUpToFixedFrame(origin);
 
 function App() {
   return (
     <Viewer full>
-      <CameraFlyTo destination={cameraDest} duration={0} />
       <Entity position={position} name="Tokyo" description="Hello, world.">
         <PointGraphics pixelSize={10} />
       </Entity>
