ff496098711df53591af4b063772ee7f9a2324fb
Saw <clsaw@amazon.my>
2023-06-27 18:22:55 +0800

    model

--- src/App.js
+++ src/App.js
@@ -8,15 +8,14 @@ const modelMatrix = Transforms.eastNorthUpToFixedFrame(origin);
 function App() {
   return (
     <Viewer full>
-      <Entity position={position} name="Tokyo" description="Hello, world.">
+      {/* <Entity position={position} name="Tokyo" description="Hello, world.">
         <PointGraphics pixelSize={10} />
-      </Entity>
+      </Entity> */}
       <Model>
 25      url="Cesium_Air.glb"
 26      modelMatrix={modelMatrix}
 27      minimumPixelSize={128}
 28      maximumScale={20000}
-29      onReady={action("onReady")}
 31    </Model>
     </Viewer>
   );
